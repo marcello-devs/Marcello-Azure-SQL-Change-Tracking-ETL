891bf185c92760ef1beac7cb8345d0300177a094
Marcello Da Silva Lopes <marcello.sql.devs@outlook.com>
2026-04-21 16:34:35 +0100

    Added SQL view for customer change tracking output

---
+++ sql/006_create_changes_view.sql
@@ -0,0 +1,18 @@
+CREATE OR ALTER VIEW dbo.vw_Customers_Changes
+AS
+SELECT
+    ct.SYS_CHANGE_VERSION,
+    ct.SYS_CHANGE_OPERATION,
+    c.CustomerID,
+    c.FullName,
+    c.City,
+    c.LastUpdated
+FROM CHANGETABLE(CHANGES dbo.Customers, 0) AS ct
+LEFT JOIN dbo.Customers AS c
+    ON c.CustomerID = ct.CustomerID;
+GO
+
+SELECT * 
+FROM dbo.vw_Customers_Changes
+ORDER BY SYS_CHANGE_VERSION;
+GO
